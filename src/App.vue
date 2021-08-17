595dca2eabebbb692fad45ea024d8ccb9e079ec2
Jesica Sieiro <jesica.sieiro@gmail.com>
2021-08-17 17:57:41 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,83 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-dark bg-primary">
-      <a href="/" class="navbar-brand">URL Database</a>
+
+  <div >
+    <nav class="navbar .navbar-dark" style="background-color: #191654; color : white ; text-align:center">
+      <a href="/" class="navbar-brand " style="color : white ; padding : 20px ;" >Jesica Romina Sieiro</a>
     </nav>
     <!--MAIN CONTENT-->
     <div class="container">
-      <div class="row mt-5">
-        <div class="col-sm-4">
-          <div class="card">
+      <div class="row mt-3">
+        <div class="col-sm-5 text-center">
+          <!--NUEVO-->
+          <!--<div class="card" style="width: 15rem;">
+              <img src="./assets/fotoPorfolio.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p>
+                Buenas! Soy Licenciada en Gestión de Tecnología y lo que más me apasiona es aprender continuamente nuevas tecnologías e ir probando cada una de ellas.
+                A la derecha les muestro una de las cosas que hice, un blog en el cual voy subiendo contenido teorico y mi repositorio de git.  
+              </p>
+              <p>
+                Muchisimas gracias por haber entrado y ojalá podamos conocernos y aprender juntos.
+              </p>
+              </div>
+          </div>-->
+
+          <img  src="./assets/fotoPorfolio.jpg" class="foto-presentacion" alt="Foto de Jesica Sieiro">
+
+          <div class="presentacion">
+              <p>
+                Buenas! Soy Licenciada en Gestión de Tecnología y lo que más me apasiona es aprender continuamente nuevas tecnologías e ir probando cada una de ellas.
+                A la derecha les muestro una de las cosas que hice, un blog en el cual voy subiendo contenido teorico y mi repositorio de git.  
+              </p>
+              <p>
+                Muchisimas gracias por haber entrado y ojalá podamos conocernos y aprender juntos.
+              </p>
+          </div>
+
+         <!-- <div class="card" style="margin: 30px 0px">
+          
+            <div class="card-body" >
+               <p>
+                Buenas! Soy Licenciada en Gestión de Tecnología y lo que más me apasiona es aprender continuamente nuevas tecnologías e ir probando cada una de ellas.
+                A la derecha les muestro una de las cosas que hice, un blog en el cual voy subiendo contenido teorico y mi repositorio de git.  
+              </p>
+              <p>
+                Muchisimas gracias por haber entrado y ojalá podamos conocernos y aprender juntos.
+              </p>
+            </div>
+          </div>-->
+          
+          <!--FIN DE NUEVO-->
+
+          <!--CARD PARA AGREGAR-->
+          
+           <div class="card" v-if="ocultar">
             <div class="card-header">
               <h3>Agrega un nuevo Sitio Web</h3>
             </div>
             <div class="card-body">
               <form @submit.prevent="addWebsite">
                 <div class="form-group">
-                  <input type="text" class="form-control" 
+                  <input type="text" class="form-control"
+                    id="name" 
                     v-model="newWebsite.name"
                     placeholder="Nombre del sitio">
                 </div>
                 <div class="form-group">
                   <input type="text" class="form-control" 
-                    v-model="newWebsite.author"
-                    placeholder="Autor del sitio">
+                    id="description"
+                    v-model="newWebsite.description"
+                    placeholder="Descripción del sitio">
                 </div>
                 <div class="form-group">
                   <input type="text" class="form-control" 
+                    id="tecnology"
+                    v-model="newWebsite.tecnology"
+                    placeholder="Tecnología utilizada">
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" 
+                    id="url"
                     v-model="newWebsite.url"
                     placeholder="Dirección URL del sitio">
                 </div>
@@ -35,35 +88,57 @@
             </div>
           </div>
         </div>
-        <div class="col-sm-8 text-center">
-           <img src="./assets/logo.png">
-           <div class="card">
+
+        <!--FIN DE CARD DE AGREGAR-->
+
+
+        <div class="col-sm-7 text-center">
+          <!--<img src="./assets/fotoPorfolio.jpg" style="width: 10rem; margin:20px" alt="...">-->
+           <!--<img src="./assets/logo.png">-->
+           <div class="card tarjeta" >
              <div class="card-header">
                <h3>Lista de Sitios Web</h3>
              </div>
              <div class="card-body">
-               <table class="table table-striped table-bordered">
+               <table class="table  table-hover blanco"  >
                  <thead>
                    <tr>
                      <th>Nombre</th>
-                     <th>Autor</th>
-                     <th>Operaciones</th>
+                     <th>Descripción</th>
+                     <th>Tecnología usada</th>
+                     <th v-if="ocultar">Operaciones</th>
+                     <th v-else>Visitar</th>
+                     
                    </tr>
                    
                  </thead>
                  <tbody>
                    <tr v-for="(w, index) in websites" v-bind:key="index['.id']">
                      <td >
-                       <a v-bind:href="w.url" target="_blank">{{w.name}}</a>
+                       <a class="blanco" v-bind:href="w.url" target="_blank">{{w.name}}</a>
                      </td>
                      <td>
-                       {{w.author}}
+                       {{w.description}}
                      </td>
                      <td>
-                       <button class="btn btn-danger" @click="deleteWebsite(index)">
+                       {{w.tecnology}}
+                     </td>
+                     <td v-if="ocultar">
+                       <button class="btn btn-danger" @click="deleteWebsite(index)" >
                          Delete
                        </button>
+                      
+                       <!-- prueba para agregar btn de editar 
+                       <button class="btn btn-danger" @click="editar(index,w)">
+                         Edit
+                       </button>-->
                      </td>
+                     <td v-else>
+                        <a class="btn btn-success" v-bind:href="w.url" target="_blank" >
+                         Ir
+                       </a>
+                     </td>
+                     
                    </tr>
                  </tbody>
                </table>
@@ -103,18 +178,22 @@ export default {
   //la propiedad data te permite modelar el dato que vas a poder almacenar
   //desde tu tamplate
   data(){
+    
     //en este caso data es una funcion 
     //donde retornamos un modelo de sitio web que va a ser un objeto
     return {
       websites:[],
       newWebsite:{
         name:'',
-        author:'',
-        url:''
-      }
+        description:'',
+        url:'',
+        tecnology:'',
+      },
+     ocultar:false
     }
 
   },
+  
   //NUEVO
   created() {
              Firebase.database().ref('websites').on('value', (snapshot) => {
@@ -129,8 +208,9 @@ export default {
       websitesRef.push(this.newWebsite);
       toastr.success("El sitio fue agregado a la lista");
       this.newWebsite.name='';
-      this.newWebsite.author='';
+      this.newWebsite.description='';
       this.newWebsite.url='';
+      this.newWebsite.tecnology='';
     },
     //recibe por parametro el elemeto que es uno de los sitios de la tabla
     deleteWebsite(index){
@@ -151,17 +231,59 @@ export default {
           toastr.success("El sitio fue eliminado de la lista");
         }
         
-    }
+    },
+
+
+    //EDITAR -- queda por hacer
+    /*editar(index,ws){
+      //obtener datos del objeto
+      
+      //llenar inputs con datos del objeto
+      //inhabilitar btn guardar
+      //poner boton Modificar
+      //obtener datos de los inputs
+      //guardarlos en firebase
+    }*/
     
-  }
+  },
+ 
+
+  
 }
+
+
 </script>
 
 <style>
-#app {
-  background: #1a2a6c;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #fdbb2d, #b21f1f, #1a2a6c);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #fdbb2d, #b21f1f, #1a2a6c); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  height: 100vh;
+a{
+  text-decoration: none;
+}
+
+.blanco{
+  color: white
+}
+
+.tarjeta{
+  background: transparent; color : white ; border-color : white;
+}
+
+.presentacion{
+   color: white; margin : 20px; padding: 10px; text-align : left ;
+}
+.foto-presentacion{
+  width: 10rem; border-radius: 50% ; border: solid 2px #191654
+}
+
+@media(max-width: 767px){
+  body{
+    height: 100%;
+  }
+  #app {
+
+        background: #43C6AC;  /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #191654, #43C6AC);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to right, #191654, #43C6AC); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        height: 100vh;
+  }
 }
 </style>
--- src/config.js
+++ src/config.js
@@ -1,10 +1,18 @@
 export default{
-    apiKey: "AIzaSyDNiFrRW2aBZDkKJFw7F24QywAHSRyorvI",
+    /*apiKey: "AIzaSyDNiFrRW2aBZDkKJFw7F24QywAHSRyorvI",
     authDomain: "bdd-listaurl-bbabe.firebaseapp.com",
     databaseURL: "https://bdd-listaurl-bbabe-default-rtdb.firebaseio.com",
     projectId: "bdd-listaurl-bbabe",
     storageBucket: "bdd-listaurl-bbabe.appspot.com",
     messagingSenderId: "393296731813",
     appId: "1:393296731813:web:47bebbff791b2ccf99774c",
-    measurementId: "G-LLTT6BNMYV"
+    measurementId: "G-LLTT6BNMYV"*/
+    apiKey: "AIzaSyCSjs_HTpT_T0kjY9Mz09yspFbJpaP92vY",
+    authDomain: "porfoliojsieiro.firebaseapp.com",
+    databaseURL: "https://porfoliojsieiro-default-rtdb.firebaseio.com",
+    projectId: "porfoliojsieiro",
+    storageBucket: "porfoliojsieiro.appspot.com",
+    messagingSenderId: "957046626091",
+    appId: "1:957046626091:web:873b650950b3d9af28576e",
+    measurementId: "G-849MCWJDQR"
 }
